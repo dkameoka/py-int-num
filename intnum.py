c56a430380fadadbb5308df91a1d8a546b778fd8
Darren Kameoka <48581401+dkameoka@users.noreply.github.com>
2023-01-19 12:54:44 -1000

    style: cleanup

--- intnum.py
+++ intnum.py
@@ -57,8 +57,7 @@ class IntegerToEnglishLatin:
             if tens == 1:
                 result.append(self.ENG_TABLE[units].teens)
             elif units > 0:
-                result.append(
-                    self.ENG_TABLE[tens].tens + '-' + self.ENG_TABLE[units].units)
+                result.append(self.ENG_TABLE[tens].tens + '-' + self.ENG_TABLE[units].units)
             else:
                 result.append(self.ENG_TABLE[tens].tens)
         else:
@@ -76,8 +75,9 @@ class IntegerToEnglishLatin:
             if hundreds == 1 and tens == 0 and units == 3:  # Special case for 103
                 result.insert(0, 'trescentilli')
                 continue
-            part = self.LAT_TABLE[units].units + \
-                self.LAT_TABLE[tens].tens + self.LAT_TABLE[hundreds].hundreds
+            part = (self.LAT_TABLE[units].units +
+                    self.LAT_TABLE[tens].tens +
+                    self.LAT_TABLE[hundreds].hundreds)
             if part[-1] in ['a', 'e', 'o']:  # Replace some endings
                 part = part[:-1] + 'i'
             part += 'lli'
@@ -101,6 +101,5 @@ class IntegerToEnglishLatin:
                 result.append(numeral)
                 continue
             short_numeric_scale = triplet_index - 1
-            result.insert(0, numeral + ' ' +
-                          self._latin_from_short_scale(short_numeric_scale))
+            result.insert(0, numeral + ' ' + self._latin_from_short_scale(short_numeric_scale))
         return negative + ' '.join(result)
