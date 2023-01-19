e3546e19dfe0f1822a5df1f641327fbc24835369
Darren Kameoka <48581401+dkameoka@users.noreply.github.com>
2023-01-18 19:40:57 -1000

    refactor: utilize dataclasses and attributes

--- intnum.py
+++ intnum.py
@@ -1,9 +1,50 @@
 #!/usr/bin/env python3
 
 import itertools
+from dataclasses import dataclass
+
+
+@dataclass
+class _EnglishNumeral:
+    units: str
+    teens: str
+    tens: str
+
+
+@dataclass
+class _LatinNumeral:
+    simplex: str
+    units: str
+    tens: str
+    hundreds: str
 
 
 class IntegerToEnglishLatin:
+
+    ENG_NUM_TABLE = {
+        0: _EnglishNumeral('',      'ten',       ''),
+        1: _EnglishNumeral('one',   'eleven',    ''),
+        2: _EnglishNumeral('two',   'twelve',    'twenty'),
+        3: _EnglishNumeral('three', 'thirteen',  'thirty'),
+        4: _EnglishNumeral('four',  'fourteen',  'forty'),
+        5: _EnglishNumeral('five',  'fifteen',   'fifty'),
+        6: _EnglishNumeral('six',   'sixteen',   'sixty'),
+        7: _EnglishNumeral('seven', 'seventeen', 'seventy'),
+        8: _EnglishNumeral('eight', 'eighteen',  'eighty'),
+        9: _EnglishNumeral('nine',  'nineteen',  'ninety')}
+
+    LAT_NUM_TABLE = {
+        0: _LatinNumeral('n',     '',         '',             ''),
+        1: _LatinNumeral('m',     'un',       'deci',         'centi'),
+        2: _LatinNumeral('b',     'duo',      'viginti',      'ducenti'),
+        3: _LatinNumeral('tr',    'tre',      'triginta',     'trecenti'),
+        4: _LatinNumeral('quadr', 'quattuor', 'quadraginta',  'quadringenti'),
+        5: _LatinNumeral('quint', 'quin',     'quinquaginta', 'quingenti'),
+        6: _LatinNumeral('sext',  'sex',      'sexaginta',    'sescenti'),
+        7: _LatinNumeral('sept',  'septen',   'septuaginta',  'septingenti'),
+        8: _LatinNumeral('oct',   'octo',     'octoginta',    'octingenti'),
+        9: _LatinNumeral('non',   'novem',    'nonaginta',    'nongenti')}
+
     def __init__(self, number):
         assert isinstance(number, int)
         self.number = number
@@ -13,56 +54,34 @@ class IntegerToEnglishLatin:
         return itertools.zip_longest(num_iter, num_iter, num_iter, fillvalue=0)
 
     def _english_cardinal_numeral(self, units, tens, hundreds):
-        cardinal_table = {
-            0: ['', 'ten', ''],
-            1: ['one', 'eleven', ''],
-            2: ['two', 'twelve', 'twenty'],
-            3: ['three', 'thirteen', 'thirty'],
-            4: ['four', 'fourteen', 'forty'],
-            5: ['five', 'fifteen', 'fifty'],
-            6: ['six', 'sixteen', 'sixty'],
-            7: ['seven', 'seventeen', 'seventy'],
-            8: ['eight', 'eighteen', 'eighty'],
-            9: ['nine', 'nineteen', 'ninety']}
         result = []
         if hundreds > 0:
-            result.append(cardinal_table[hundreds][0] + ' hundred')
+            result.append(self.ENG_NUM_TABLE[hundreds].units + ' hundred')
         if tens > 0:
             if tens == 1:
-                result.append(cardinal_table[units][1])
+                result.append(self.ENG_NUM_TABLE[units].teens)
             elif units > 0:
                 result.append(
-                    cardinal_table[tens][2] + '-' + cardinal_table[units][0])
+                    self.ENG_NUM_TABLE[tens].tens + '-' + self.ENG_NUM_TABLE[units].units)
             else:
-                result.append(cardinal_table[tens][2])
+                result.append(self.ENG_NUM_TABLE[tens].tens)
         else:
-            result.append(cardinal_table[units][0])
+            result.append(self.ENG_NUM_TABLE[units].units)
         return ' '.join(filter(len, result))
 
     def _latin_from_short_scale(self, exp_short_scale):
-        cg_table = {
-            0: ['n', '', '', ''],
-            1: ['m', 'un', 'deci', 'centi'],
-            2: ['b', 'duo', 'viginti', 'ducenti'],
-            3: ['tr', 'tre', 'triginta', 'trecenti'],
-            4: ['quadr', 'quattuor', 'quadraginta', 'quadringenti'],
-            5: ['quint', 'quin', 'quinquaginta', 'quingenti'],
-            6: ['sext', 'sex', 'sexaginta', 'sescenti'],
-            7: ['sept', 'septen', 'septuaginta', 'septingenti'],
-            8: ['oct', 'octo', 'octoginta', 'octingenti'],
-            9: ['non', 'novem', 'nonaginta', 'nongenti']}
         if exp_short_scale < 1:
             return 'thousand'
         result = []
         for units, tens, hundreds in self._triplets(exp_short_scale):
             if hundreds == 0 and tens == 0:
-                result.insert(0, cg_table[units][0] + 'illi')
+                result.insert(0, self.LAT_NUM_TABLE[units].simplex + 'illi')
                 continue
             if hundreds == 1 and tens == 0 and units == 3:  # Special case for 103
                 result.insert(0, 'trescentilli')
                 continue
-            part = cg_table[units][1] + \
-                cg_table[tens][2] + cg_table[hundreds][3]
+            part = self.LAT_NUM_TABLE[units].units + \
+                self.LAT_NUM_TABLE[tens].tens + self.LAT_NUM_TABLE[hundreds].hundreds
             if part[-1] in ['a', 'e', 'o']:  # Replace some endings
                 part = part[:-1] + 'i'
             part += 'lli'
